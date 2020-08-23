7ef0678ecc659e8c902db5b7239e06eef5793bc5
manojks1999 <64463831+manojks1999@users.noreply.github.com>
2020-08-23 19:41:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+import math
+
+app = Flask(__name__)
+
+model=pickle.load(open('model.pkl','rb'))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[float(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(final)
+    prediction=model.predict(final)
+    output=math.floor(prediction[0])
+
+    print(output)
+
+    if output:
+        return render_template('index.html',pred='You can buy this laptop at {} Rs '.format(str(output)))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
